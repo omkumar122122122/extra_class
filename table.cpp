c2946cd585b10046ebcab1590d6ced1ef691aa3e
omkumar122122122 <omkumar122122122@gmail.com>
2025-08-28 15:02:28 +0530

    update

--- table.cpp
+++ table.cpp
@@ -3,9 +3,10 @@ using namespace std;
 int main(){
     int n,m;
     cout<<"enter no.to print table";
-    for(int i=1,i<11;i++){
+    cin>>n;
+    for(int i=1;i<11;i++){
         m=n*i;
-        cout<<1<<"*"<<n<<"="<<m;
+        cout<<i<<"*"<<n<<"="<<m<<endl;
     }
     
           return 0;
